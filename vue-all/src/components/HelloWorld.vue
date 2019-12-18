b7a6a671cdfb7a4ee91a102b20a60e4e4685025a
haiyan <qihaiyan@hjzddata.com>
2019-12-18 15:06:31 +0800

    project init

---
+++ vue-all/src/components/HelloWorld.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="hello">
+    {{val}}
+    <v-input @input="val = arguments[0]" :value='val' />
+  </div>
+</template>
+
+<script>
+import VInput from './Input.vue';
+
+export default {
+  name: 'HelloWorld',
+  components: { VInput },
+  data(){
+    return {
+      val: '',
+    }
+  },
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ vue-all/src/components/Input.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <!-- v-model 双向数据绑定 
+            1、绑定value值
+            2、监听input事件
+         -->
+         <input type="text" @input='onInput' :value="value"/>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'KInput',
+    props: ['value'],
+    methods: {
+        onInput (e) {
+            let val = e.target.value;
+            this.$emit('input', val);
+        }
+    },
+}
+</script>
